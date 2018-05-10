7147cdda8e2dac3a14c767480d65499a3d8abc6f
GlaucioMendonca <glaucio43@hotmail.com>
2018-05-10 10:35:58 -0300

    Criando repositorio pp
    Criação de pasta individual e adição do projeto observer

--- Padroes de projeto/Atividade 1 - Baralho/Jogo de baralho/src/model/Algoritimo.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface Algoritimo {
-	
-	public ArrayList<Carta> distribuirCartas (ArrayList<Carta> baralho); 
-}
--- Padroes de projeto/Atividade 2 - Padroes de responsabilidade/src/model/CalculaValorAluguel.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface CalculaValorAluguel {
-	
-	public Double calcular(int dias);
-}
